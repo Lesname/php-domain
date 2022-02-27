e81a1465946aaebdc75b71452d79b44f83a01232
mkroeders <mkroeders@gmail.com>
2022-02-27 15:55:25 +0100

    Added IdentifierService::generate

---
+++ src/Identifier/Uuid6IdentifierService.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace LessDomain\Identifier;
+
+use LessValueObject\String\Exception\TooLong;
+use LessValueObject\String\Exception\TooShort;
+use LessValueObject\String\Format\Exception\NotFormat;
+use LessValueObject\String\Format\Resource\Identifier;
+use Ramsey\Uuid\Uuid;
+
+final class Uuid6IdentifierService implements IdentifierService
+{
+    /**
+     * @throws TooLong
+     * @throws TooShort
+     * @throws NotFormat
+     */
+    public function generate(): Identifier
+    {
+        return new Identifier((string)Uuid::uuid6());
+    }
+}
